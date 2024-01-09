4055cdbb265546cdd01d5b5226fe7c203c285b95
SimiyuWafulah <simiyukelvinse@gmail.com>
2024-01-09 17:33:39 +0300

    created signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,9 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const signup = async (req, res, next) => {
   try {
@@ -13,4 +17,22 @@ export const signup = async (req, res, next) => {
   } catch (error) {
     next(error)
   }
+}
+
+export const signin = async (req, res, next) => {
+    try {
+       const {email, password} = req.body;
+       const validUser = await User.findOne({email});
+       if(!validUser) return next(errorHandler(404, 'Account does not exist'));
+       const validPassword = bcryptjs.compareSync(password, validUser.password);
+       if(!validPassword) return next(errorHandler(404,'Wrong Credentials')); 
+       const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET_KEY);
+       const {password: pass, ...rest} = validUser._doc
+       res
+       .cookie('access_token', token , {httpOnly: true})
+       .status(200)
+       .json({message: 'Signed In Successfully'})
+    } catch (error) {
+        next(error)
+    }
 }
